54129280a9cf46fdae19e74ae8f21342b9826487
Kevin Lopez <klopeze14@miumg.edu.gt>
2025-05-28 10:45:35 -0600

    Crud de archivos

--- Proyecto de Modernización de Tecnología/main.cpp
+++ Proyecto de Modernización de Tecnología/main.cpp
@@ -2,13 +2,10 @@
 #include <iostream>
 #include <fstream>
 #include "Usuario.h"
-#include "Inventario.h"
 #include "Bitacora.h"
-#include "Menu.h"
 using namespace std;
 
 usuarios usuarioRegistrado;
-Menu menu;
 
 int main() {
         // Verificar si el archivo binario de usuarios existe
@@ -40,59 +37,32 @@ int main() {
         return 0;
     }
 
-    Inventario inventario;
-    Facturacion factura;
     bitacora bit;
     string nombreUsuario = usuarioRegistrado.getNombre();
 
     int opcionGeneral;
     do {
-        system("cls");
         cout << "\n=========== MENÚ GENERAL ===========\n";
         cout << "Usuario: " << nombreUsuario << "\n";
         cout << "1. Catalogo\n";
-        cout << "2. Procesos\n";
-        cout << "3. Informes\n";
+        cout << "2. Informe\n";
+        cout << "3. Seguridad\n";
         cout << "4. Salir de sesion\n";
         cout << "====================================\n";
         cout << "Seleccione una opción: ";
         cin >> opcionGeneral;
 
+
         switch (opcionGeneral) {
             case 1:
-                menu.mostrarOpciones(inventario, factura, nombreUsuario);
-                bit.insertar(nombreUsuario, 1999, "Catálogo", "Acceso al submenú de inventario");
+                usuarioRegistrado.menuUsuarios();
+                bit.insertar(nombreUsuario, 1, "Catálogo", "Acceso al submenú de Catalogos");
                 break;
 
             case 2: {
                 int opcionProcesos;
-                do {
-                    system("cls");
-                    cout << "\n=========== MENÚ DE PROCESOS ===========\n";
-                    cout << "1. Facturacion\n";
-                    cout << "2. Gestion de usuarios\n";
-                    cout << "3. Volver al menú general\n";
-                    cout << "========================================\n";
-                    cout << "Seleccione una opción: ";
-                    cin >> opcionProcesos;
-
-                    switch (opcionProcesos) {
-                        case 1:
-                            factura.generarFactura(inventario);
-                            bit.insertar(nombreUsuario, 2101, "Procesos", "Acceso a facturación");
-                            break;
-                        case 2:
-                            usuarioRegistrado.menuUsuarios();
-                            bit.insertar(nombreUsuario, 2102, "Procesos", "Acceso a gestión de usuarios");
-                            break;
-                        case 3:
-                            cout << "Volviendo al menú general...\n";
-                            break;
-                        default:
-                            cout << "Opción inválida. Intente nuevamente.\n";
-                            system("pause");
-                    }
-                } while (opcionProcesos != 3);
+                usuarioRegistrado.consultarUsuarios();
+                bit.insertar(nombreUsuario, 2, "Informes", "Revisión de informes");
                 break;
             }
 
